b370920398e5448a87d8f3250284bef77dcf9c67
Jacob Choi <jchoi2507@gmail.com>
2023-02-28 23:31:52 -0500

    Update ESP8266_CrazyCar.py

--- Crazy_Car/ESP8266_CrazyCar.py
+++ Crazy_Car/ESP8266_CrazyCar.py
@@ -1,3 +1,14 @@
+'''
+
+ESP8266_CrazyCar.py
+
+MicroPython script loaded onto ESP8266
+
+1) Publishes a command to an MQTT topic
+Note: MQTT setup and connection is performed on boot, in ESP8266_boot.py
+
+'''
+
 from secrets import *
 from serial import *
 import mqtt_CBR
@@ -9,21 +20,15 @@ topicSub = 'ESP/listen'
 topicPub = 'ESP/tell'
 clientID = 'Jacob_Suely_ESP'
 
-def whenCalled(topic, msg):
-    print((topic.decode(), msg.decode()))
-    blink()
-    time.sleep(0.5)
-    blink()
-
-mqtt_CBR.connect_wifi(Home_WiFi)
-client = mqtt_CBR.mqtt_client(clientID, mqttBroker, whenCalled)
-
 def main():
     s = serial_comm(115200)
-    s.abort()
-    while True:
-        if (s.any()):
-            command = s.read()
-            client.publish(topicPub, command)
-            break
-        time.sleep(0.5)
+    #s.abort()
+    #while True:
+    #    if (s.any()):
+    #        print("DATA RECEIVED\n")
+    #        command = s.read()2
+    #        client.publish(topicPub, command)
+    #        break
+    time.sleep(0.5)
+
+main()
